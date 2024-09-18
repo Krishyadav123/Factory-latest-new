df12a80786d31004dceeb46789487a20a5f582dd
krishyadav123 <krishyadav28494@gmail.com>
2024-09-18 18:08:37 +0530

    product

--- src/Components/Products/Product.jsx
+++ src/Components/Products/Product.jsx
@@ -4,6 +4,7 @@ import midimg from '../../assets/mid-imae.jpg'
 import { Link, useNavigate } from 'react-router-dom'
 import { FactoryContext } from '../../Context/Context'
 // import image01 from '../../assets/image01.jpg'
+import product01 from '../../assets/Product01.jpg'
 import product01new from '../../assets/product01new.jpg'
 import product02new from '../../assets/product02new.jpg'
 import product03new from '../../assets/product03new.jpg'
@@ -92,12 +93,22 @@ const Product = () => {
         <div className="product-cards">
             <div className="product-card-one" onClick={() => handleProductClick(1)}>
               <div className="product-card-img">
-                <img src={product01new} alt="" />
+                <img src={product01} alt="" />
               </div>
               <div className="product-card-text">
                 <h2>
                 STONE WOOL SECTIONAL PIPE INSULATION.</h2>
                 <p>Shreeinsul Stone Wool Sectional Pipe Insulation is designed for effective thermal and acoustic insulation of pipes in various applications... </p>
+                <hr />
+              </div>
+            </div>
+            <div className="product-card-one" onClick={() => handleProductClick(3)}>
+              <div className="product-card-img">
+                <img src={product03new} alt="" />
+              </div>
+              <div className="product-card-text">
+                <h2>STONE WOOL RESIN BONDED BOARDS OR SLABS</h2>
+                <p>Discover the superior performance of Shreeinsul Stone Wool Resin Bonded Boards! Ideal for thermal insulation, acoustic treatment, and fire protection...</p>
                 <hr />
               </div>
             </div>
@@ -109,16 +120,6 @@ const Product = () => {
               <div className="product-card-text">
                 <h2>STONE WOOL BLANKET (LRB MATTRESS)</h2>
                 <p>Shreeinsul Stone Wool Blankets are designed to meet rigorous industry standards, ensuring superior performance across various...</p>
-                <hr />
-              </div>
-            </div>
-            <div className="product-card-one" onClick={() => handleProductClick(3)}>
-              <div className="product-card-img">
-                <img src={product03new} alt="" />
-              </div>
-              <div className="product-card-text">
-                <h2>STONE WOOL RESIN BONDED BOARDS OR SLABS</h2>
-                <p>Discover the superior performance of Shreeinsul Stone Wool Resin Bonded Boards! Ideal for thermal insulation, acoustic treatment, and fire protection...</p>
                 <hr />
               </div>
             </div>
