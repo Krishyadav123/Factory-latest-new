45e369f3f69a3b2310af8f06418c0799e1b7b11d
Tech-Avrach <nikhilpatel9226@gmail.com>
2024-11-07 12:54:05 +0530

    css update

--- src/Components/News/News.jsx
+++ src/Components/News/News.jsx
@@ -50,64 +50,64 @@ const News = () => {
 
 
   return (
-    <div class="news">
+    <div className="news">
      <div><h1>NEWS</h1></div>
 <div className='news-artical'>  
-    <article class="card">
+    <article className="card">
   <img
-    class="card__background"
+    className="card__background"
     src={image01}
     alt="Photo of Cartagena's cathedral at the background and some colonial style houses"
     width="1920"
     height="2193"
   />
-  <div class="card__content | flow">
-    <div class="card__content--container | flow">
-      <h2 class="card__title">Colombia</h2>
-      <p class="card__description">
+  <div className="card__content | flow">
+    <div className="card__content--container | flow">
+      <h2 className="card__title">Colombia</h2>
+      <p className="card__description">
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum in
         labore laudantium deserunt fugiat numquam.
       </p>
     </div>
-    <button class="card__button">Read more</button>
+    <button className="card__button">Read more</button>
   </div>
 </article>
-    <article class="card">
+    <article className="card">
   <img
-    class="card__background"
+    className="card__background"
     src={image02}
     alt="Photo of Cartagena's cathedral at the background and some colonial style houses"
     width="1920"
     height="2193"
   />
-  <div class="card__content | flow">
-    <div class="card__content--container | flow">
-      <h2 class="card__title">Colombia</h2>
-      <p class="card__description">
+  <div className="card__content | flow">
+    <div className="card__content--container | flow">
+      <h2 className="card__title">Colombia</h2>
+      <p className="card__description">
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum in
         labore laudantium deserunt fugiat numquam.
       </p>
     </div>
-    <button class="card__button">Read more</button>
+    <button className="card__button">Read more</button>
   </div>
 </article>
-    <article class="card">
+    <article className="card">
   <img
-    class="card__background"
+    className="card__background"
     src={image03}
     alt="Photo of Cartagena's cathedral at the background and some colonial style houses"
     width="1920"
     height="2193"
   />
-  <div class="card__content | flow">
-    <div class="card__content--container | flow">
-      <h2 class="card__title">Colombia</h2>
-      <p class="card__description">
+  <div className="card__content | flow">
+    <div className="card__content--container | flow">
+      <h2 className="card__title">Colombia</h2>
+      <p className="card__description">
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum in
         labore laudantium deserunt fugiat numquam.
       </p>
     </div>
-    <button class="card__button">Read more</button>
+    <button className="card__button">Read more</button>
   </div>
 </article></div>
     </div>
